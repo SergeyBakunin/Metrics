adf5233ed28212fd24720bf698730e5b3e540d3b
SergeyBakunin <104261138+SergeyBakunin@users.noreply.github.com>
2022-10-12 04:09:21 +0300

    Добавьте файлы проекта.

---
+++ Metrics_Agent/Controllers/CpuMetricsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Metrics_Agent.Controllers
+{
+    [Route("api/metrics/cpu")]
+    [ApiController]
+    public class CpuMetricsController : ControllerBase
+    {
+
+        [HttpGet("from/{fromTome}/to/{toTime}")]
+        public IActionResult GetCpuMetrics([FromRoute]TimeSpan fromrTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ Metrics_Agent_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Metrics_Agent_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
